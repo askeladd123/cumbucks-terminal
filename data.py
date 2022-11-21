408a336995b227404f0d91f372dfd0ae1a7e8d88
Ask Sødal <asks@uia.no>
2022-11-21 18:57:26 +0100

    klar for github

--- data.py
+++ data.py
@@ -1,27 +1,31 @@
 
-work_hard = ["fysikk", "algoritmer", "software"]
-work_easy = ["ai", "skjær"]
-tasks_hard = ["timeliste", "meta-analyse"]
+work_hard = ["fysikk", "algoritmer", "ai", "software"]
+work_easy = ["ai", "skjær", "ask game engine"]
+tasks_hard = ["timeliste", "planlegge Oslo"]
 tasks_easy = [
-    "finne eske til Q35",
     "finne høytaler",
     "push-ups, 50",
     "pull-ups, 24",
-    "zalando, 20 min"
-    "oppdater plan"
+    "curls, 40",
+    "zalando, 20 min",
+    "oppdater plan",
+    "finne dress",
 ]
 rewards_small = [
     "piano",
     "gitar",
     "mw2, 1 runde",
-    "paranoid android",
     "snacks",
     "pause 10 min",
-    "weird fishes",
-    "youtube, 1 video"
+    "funky drummer",
+    "popcorn - james brown",
+    "burial",
+    "youtube, 1 video",
+    "pudding",
 ]
 rewards_big = [
-    "better call saul, 1ep",
+    "pudding og youtube",
+    "rick and morty, 1 ep",
     "mw2, 3 runder",
     "pause 30 min",
     "pause 20 min"
